47749cc8b09735723e99acade12342867b5ca2b5
Rina <rina.ahmed@live.com>
2018-12-27 15:57:34 +0100

    Removing Values Integration

--- PeopleApp-SPA/src/app/register/register.component.ts
+++ PeopleApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 
 @Component({
@@ -7,7 +7,6 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
