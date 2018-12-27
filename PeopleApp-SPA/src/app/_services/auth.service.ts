d0aee879f17f8fe8af931e9bba01e7778cacd8f7
Rina <rina.ahmed@live.com>
2018-12-27 15:54:19 +0100

    addiding registration in frontend

--- PeopleApp-SPA/src/app/_services/auth.service.ts
+++ PeopleApp-SPA/src/app/_services/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
 
 constructor(private http: HttpClient) { }
 
-login(model: any){
+login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
     .pipe(
       map((response: any) => {
@@ -22,6 +22,10 @@ login(model: any){
         }
       })
     );
+}
+
+register(model: any) {
+  return this.http.post(this.baseUrl + 'register', model);
 }
 
 }
