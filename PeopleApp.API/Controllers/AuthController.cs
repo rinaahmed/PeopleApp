abb83e96c4aff5625ea3e382bc7e8328bc5a73de
Rina <rina.ahmed@live.com>
2019-01-03 20:56:07 +0100

    Handling errors in Angular

--- PeopleApp.API/Controllers/AuthController.cs
+++ PeopleApp.API/Controllers/AuthController.cs
@@ -52,7 +52,7 @@ namespace PeopleApp.API.Controllers
         [HttpPost("login")]  
         public async Task<IActionResult> Login (UserForLoginDto userForLoginDto) 
         {
-            throw new Exception("computer says no!");
+            //throw new Exception("computer says no!");
             
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
